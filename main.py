7e090cb25017c4ded7ac519616bf3f8adf533eb0
RSGameTech <72400033+RSGameTech@users.noreply.github.com>
2021-04-09 23:02:25 -0700

    Element

--- main.py
+++ main.py
@@ -14,41 +14,43 @@ intents = discord.Intents.all()
 intents.members = True
 bot = commands.Bot(command_prefix="--", intents=intents, help_command=MinimalEmbedHelp())
 #bot.remove_command('help')
-status = cycle([discord.Game(name="Discord"),
-discord.Activity(type=discord.ActivityType.listening, name="RSGameTech"),
-discord.Activity(type=discord.ActivityType.watching, name="Youtube")])
+status = cycle([discord.Game(name="Discord"),discord.Activity(type=discord.ActivityType.listening, name="RSGameTech"),discord.Activity(type=discord.ActivityType.watching, name="Youtube")])
 
 @bot.event
 async def on_ready():
-  change_status.start()
-  channel = bot.get_channel(823836100882989108)
-  embed = discord.Embed(description = f"I am Online :sunglasses:\nPing:- {round(bot.latency * 1000)}ms")
-  await channel.send(embed=embed)
-  print("Bot is online")
+    change_status.start()
+    channel = bot.get_channel(823836100882989108)
+    embed = discord.Embed(
+        description = f"I am Online :sunglasses:\nPing:- {round(bot.latency * 1000)}ms",
+        color = 0x00CCFF
+    )
+    await channel.send(embed=embed)
+    print("Bot is online")
 
 @tasks.loop(seconds=5)
 async def change_status():
-  await bot.change_presence(activity=next(status))
+    await bot.change_presence(activity=next(status))
 
 bot.owner_ids=[699566190842085439, 730454267533459568]
 
-extensions = ['cogs.moderation',
-              'cogs.auto_mod',
-              'cogs.animation',
-              'cogs.fun',
-							'cogs.event',
-              'cogs.info',
-              'cogs.utility',
-              'cogs.api',
-              'cogs.chat'
+extentions = [
+    'cogs.moderation',
+    'cogs.auto_mod',
+    'cogs.animation',
+    'cogs.fun',
+    'cogs.event',
+    'cogs.info',
+    'cogs.utility',
+    'cogs.api',
+    'cogs.chat'
 ]
 if __name__ == '__main__':
-  for extension in extensions:
-    try:
-      bot.load_extension(extension)
-    except Exception as e:
-      print(f"Error loading {extension}", file=sys.stderr)
-      traceback.print_exc()
+    for extension in extensions:
+        try:
+            bot.load_extension(extension)
+        except Excepton as e:
+            print(f"Error loading {extension}", file=sys.stderr)
+            traceback.print_exc()
 
 bot.load_extension("jishaku")
 bot.run('NzkwODMyMjYzMjYwMDEyNTcz.X-GV8A.vRC6Mw1J3hD6p3ZyaP3bkSULc6g')
